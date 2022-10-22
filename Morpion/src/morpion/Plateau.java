20cb116373662553785edeb690563757a20ba0a3
Zaiphyr <julientribouillois37240@gmail.com>
2022-10-22 16:53:10 +0200

    Update Plateau.java

--- Morpion/src/morpion/Plateau.java
+++ Morpion/src/morpion/Plateau.java
@@ -1,5 +1,20 @@
 package morpion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plateau {
+	private static int SIZE;
+	private List<Quintuplet> QuintupletsTT = new ArrayList<Quintuplet>();
+	private List<Quintuplet> QuintupletsOpen = new ArrayList<Quintuplet>();
+	private Case tabCase[] = new Case[SIZE];
+	
+	public Plateau(int size) {
+		this.SIZE = size;
+	}
+	
+	
+	
+
 
 }
